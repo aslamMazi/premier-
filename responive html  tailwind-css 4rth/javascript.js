8f40f270df49c934d03fea3bd12f9c53fe0b616d
aslamMazi <140304229+aslamMazi@users.noreply.github.com>
2024-02-06 15:07:44 +0530

    Add files via upload

--- responive html  tailwind-css 4rth/javascript.js
+++ responive html  tailwind-css 4rth/javascript.js
@@ -1,16 +1,45 @@
-document.getElementById('hamBurger').addEventListener('click', function() {
-    // Create the hamburgerMenu div if it doesn't exist
-    let hamburgerMenu = document.getElementById('hamburgerMenu');
-    if (!hamburgerMenu) {
-      hamburgerMenu = document.createElement('div');
-      hamburgerMenu.id = 'hamburgerMenu';
-      document.body.appendChild(hamburgerMenu);
-    }
 
-    // Toggle the visibility of the hamburgerMenu
-    if (hamburgerMenu.style.display === 'block') {
-      hamburgerMenu.style.display = 'none';
-    } else {
-      hamburgerMenu.style.display = 'block';
-    }
-  });
+let hamburger = document.querySelector("#hamBurger");
+let hamburgerItems = document.querySelector("#burgerItems");
+let closeHamburger = document.querySelector(".ri-close-line");
+
+hamburger.addEventListener("click", function () {
+  hamburgerItems.style.display = "block";
+});
+
+closeHamburger.addEventListener("click", function () {
+  hamburgerItems.style.display = "none";
+});
+
+
+
+  // js for custome cursore in updates section////////////////////////
+var elem = document.querySelectorAll(".elem");
+
+elem.forEach(function(walue){
+ walue.addEventListener("mouseenter",function(){
+  walue.childNodes[5].style.opacity = 1;
+  walue.childNodes[5].style.transform = 'translate(-50%, -0%)';
+  walue.childNodes[5].style.transition = 'all 0.3s linear';
+
+ });
+
+ walue.addEventListener("mouseleave",function(){
+  walue.childNodes[5].style.opacity = 0;
+});
+walue.addEventListener("mousemove",function(dets){
+  // walue.childNodes[5].style.top = dets.y+"px";
+  walue.childNodes[5].style.left = dets.x+"px";
+
+})
+})
+
+
+
+
+//   let karsar = document.querySelector("#cursur");
+// let body = document.querySelector("body");
+// body.addEventListener("mousemove", function(dtails){
+// karsar.style.top = dtails.y+"px";
+// karsar.style.left = dtails.x+"px";
+// })
